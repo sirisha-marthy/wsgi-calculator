032bb74e969c92be76fa46747ed57532620a586d
Sirisha Marthy <sirisham@amazon.com>
2018-08-05 22:54:49 -0700

    Completed lesson4 wsgi calculator assignment

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+import traceback
 """
 For your homework this week, you'll be creating a wsgi application of
 your own.
@@ -41,31 +42,72 @@ To submit your homework:
 
 """
 
+def front_page(*args):
+    return """
+        <h2>Wsgi Calculator</h2>
+        <h4>Instructions</h4>
+        <p>Add, Subtract, Multiply, or Divide by putting your arguments
+        into the URL as different elements separated by slashes.</p>
+        <p><strong>Here are some examples:</strong>
+        <br>
+        <ul>
+          <li>http://localhost:8080/multiply/3/5   => 15<br>
+          <li>http://localhost:8080/add/23/42      => 65<br>
+          <li>http://localhost:8080/subtract/23/42 => -19<br>
+          <li>http://localhost:8080/divide/22/11   => 2<br></p>
+          </ul>
+    """	
 
 def add(*args):
     """ Returns a STRING with the sum of the arguments """
+    try:
+        total = sum(map(int, args))
+    except ValueError:
+        return "Please enter numeric values for addition"
+    return str(total)
 
-    # TODO: Fill sum with the correct value, based on the
-    # args provided.
-    sum = "0"
+def multiply(*args):
+    try:
+        total = int(args[0]) * int(args[1]) 
+    except ValueError:
+        return "Please enter numeric values for multiplication"
+    return str(total)
 
-    return sum
+def subtract(*args):
+    try:
+        total = int(args[0]) - int(args[1]) 
+    except ValueError:
+        return "Please enter numeric values for difference"
+    return str(total)
 
-# TODO: Add functions for handling more arithmetic operations.
+def divide(*args):
+    try:
+        total = int(args[0]) / int(args[1]) 
+    except ValueError:
+        return "Please enter numeric values for division"
+    except ZeroDivisionError:
+	return "ZeroDivisionError: Please provide number except zero"	 
+    return str(total)
 
 def resolve_path(path):
     """
     Should return two values: a callable and an iterable of
     arguments.
     """
-
-    # TODO: Provide correct values for func and args. The
-    # examples provide the correct *syntax*, but you should
-    # determine the actual values of func and args using the
-    # path.
-    func = add
-    args = ['25', '32']
-
+    funcs = {
+            '': front_page,
+            'add' : add,
+            'multiply' : multiply,
+            'divide' : divide,
+            'subtract' : subtract,
+            }
+    path = path.strip('/').split('/')
+    func_name = path[0]
+    args = path[1:]
+    try:
+        func = funcs[func_name]
+    except KeyError:
+        raise NameError
     return func, args
 
 def application(environ, start_response):
@@ -76,9 +118,30 @@ def application(environ, start_response):
     #
     # TODO (bonus): Add error handling for a user attempting
     # to divide by zero.
-    pass
+    headers = [('Content-type', 'text/html')]
+    try:
+        path = environ.get('PATH_INFO', None)
+        if path is None:
+            raise NameError
+        func, args = resolve_path(path)
+        body = func(*args)
+        status = "200 OK"
+    except NameError:
+        status = "404 Not Found"
+        body = "<h1>Not Found</h1>"
+    except Exception:
+        status = "500 Internal Server Error"
+        body = "<h1>Internal Server Error</h1>"
+        print(traceback.format_exc())
+    finally:
+        headers.append(('Content-length', str(len(body))))
+        start_response(status, headers)
+        return [body.encode('utf8')]
+	
 
 if __name__ == '__main__':
     # TODO: Insert the same boilerplate wsgiref simple
     # server creation that you used in the book database.
-    pass
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, application)
+    srv.serve_forever()
